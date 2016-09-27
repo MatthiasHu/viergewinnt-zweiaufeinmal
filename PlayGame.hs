741d2aba2f3df9208fe569e31652b4ca1838918b
Matthias Hutzler <matthias.ralph.hutzler@student.uni-augsburg.de>
2016-09-28 00:19:27 +0200

    decide rewritten

--- PlayGame.hs
+++ PlayGame.hs
@@ -2,20 +2,43 @@ module PlayGame where
 
 import Viergewinnt
 import Data.List
+import Data.Ord (comparing)
+import Data.Maybe (isJust)
 
-decide :: Color -> Game -> Result
-decide color state  = let 
-	next = moves state color
-	possibs = map (decide (otherColor color)) next
-	oracle = if elem (Win color) possibs then Win color else
-		if elem Draw possibs then Draw else Win (otherColor color)
-	in case check state of 
-		Just res -> res
-		Nothing -> oracle
 
-survive :: Color -> Game -> [Game]
-survive c g = filter good $ moves g c
+decide :: Color -> Game -> (Int, Result, Game)
+decide c g = (t, r, g')
+  where
+    l = decide' c g
+    (Just r, g') = head . filter (isJust . fst) $ l
+    t = length $ takeWhile (not . isJust . fst) l
+
+decide' :: Color -> Game -> [(Maybe Result, Game)]
+decide' c g =
+  repeatFirstMatch (isJust . fst) $ (check g, g) : future
   where
     c' = otherColor c
-    good :: Game -> Bool
-    good g' = not . all (== Just (Win c')) . map check $ moves g' c'
+    future =
+        map (bestFor c)
+      . transpose
+      . map (\m -> [ (fst r, m) | r<-decide' c' m ])
+      $ moves g c
+
+bestFor :: Color -> [(Maybe Result, Game)] -> (Maybe Result, Game)
+bestFor c [] = error "searching empty list of options"
+bestFor c [r] = r
+bestFor c (r1:r2:rs) =
+  if fst r1 == Just (Win c) then r1
+  else bestFor c $ maximumBy (comparing $ valueFor c . fst) [r1, r2] : rs
+  where
+    valueFor :: Color -> Maybe Result -> Int
+    valueFor Blue Nothing = 0
+    valueFor Blue (Just Draw) = 0
+    valueFor Blue (Just (Win Blue)) = 1
+    valueFor Blue (Just (Win Red)) = -1
+    valueFor Red r = - valueFor Blue r
+
+repeatFirstMatch :: (a -> Bool) -> [a] -> [a]
+repeatFirstMatch t (a:as) =
+  if t a then repeat a
+  else a : repeatFirstMatch t as
