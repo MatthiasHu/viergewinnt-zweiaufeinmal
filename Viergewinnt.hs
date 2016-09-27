ff42f5784257ab03d13ff923d6bac7bb79e4930b
Matthias Hutzler <matthias.ralph.hutzler@student.uni-augsburg.de>
2016-09-27 17:32:29 +0200

    Viergewinnt is typechecking

--- Viergewinnt.hs
+++ Viergewinnt.hs
@@ -7,14 +7,31 @@ module Viergewinnt
   , check
   ) where
 
+import Data.Array
+
 
 data Color = Red | Blue
 
 otherColor :: Color -> Color
+otherColor Red = Blue
+otherColor Blue = Red
+
+data Game = Game
+  { heights :: Array Int Int
+  , slots   :: Array (Int, Int) Color
+  }
+
+valid :: Game -> Bool
+valid g =
+  let (l, r) = bounds (heights g)
+      ((l', b), (r', t)) = bounds (slots g)
+  in l==l' && r==r' -- TODO: more invariants
 
 data Result = Win Color | Draw
 
 
 moves :: Game -> Color -> [Game]
+moves = undefined
 
 check :: Game -> Maybe Result
+check = undefined
